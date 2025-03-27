ed1beff1ad9603fe6035036cfc3d725bcc393744
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-03-28 00:53:06 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,5 +2,98 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include <cstdint>
 #include "TimedDoor.h"
+
+using ::testing::_;
+using ::testing::AtLeast;
+using ::testing::Throw;
+
+class MockTimerClient : public TimerClient {
+public:
+    MOCK_METHOD(void, Timeout, (), (override));
+};
+
+class MockDoor : public Door {
+public:
+    MOCK_METHOD(void, lock, (), (override));
+    MOCK_METHOD(void, unlock, (), (override));
+    MOCK_METHOD(bool, isDoorOpened, (), (override));
+};
+
+class TimedDoorTest : public ::testing::Test {
+protected:
+    TimedDoor* door;
+    int timeout = 1000;
+
+    void SetUp() override {
+        door = new TimedDoor(timeout);
+    }
+
+    void TearDown() override {
+        delete door;
+    }
+};
+
+TEST_F(TimedDoorTest, DoorStartsLocked) {
+    EXPECT_FALSE(door->isDoorOpened());
+}
+
+TEST_F(TimedDoorTest, UnlockOpensDoor) {
+    door->unlock();
+    EXPECT_TRUE(door->isDoorOpened());
+}
+
+TEST_F(TimedDoorTest, LockClosesDoor) {
+    door->unlock();
+    door->lock();
+    EXPECT_FALSE(door->isDoorOpened());
+}
+
+TEST_F(TimedDoorTest, TimeoutThrowsWhenOpen) {
+    door->unlock();
+    EXPECT_THROW(door->throwState(), std::runtime_error);
+}
+
+TEST_F(TimedDoorTest, NoThrowWhenClosed) {
+    EXPECT_NO_THROW(door->throwState());
+}
+
+TEST(TimerTest, RegistersClient) {
+    MockTimerClient client;
+    Timer timer;
+    EXPECT_CALL(client, Timeout()).Times(1);
+    timer.tregister(10, &client);
+}
+
+TEST(DoorTimerAdapterTest, CallsTimeout) {
+    TimedDoor door(1000);
+    DoorTimerAdapter adapter(door);
+    door.unlock();
+    EXPECT_THROW(adapter.Timeout(), std::runtime_error);
+}
+
+TEST_F(TimedDoorTest, TimerRegistrationOnUnlock) {
+    MockTimerClient client;
+    Timer timer;
+    EXPECT_CALL(client, Timeout()).Times(AtLeast(1));
+    door->unlock();
+}
+
+TEST_F(TimedDoorTest, ExceptionAfterTimeout) {
+    testing::FLAGS_gtest_death_test_style = "threadsafe";
+    door->unlock();
+    ASSERT_EXIT(
+        door->throwState(),
+        ::testing::KilledBySignal(SIGABRT),
+        "Door opened too long!"
+    );
+}
+
+TEST(MockDoorTest, VerifyLockUnlock) {
+    MockDoor door;
+    EXPECT_CALL(door, unlock()).Times(1);
+    EXPECT_CALL(door, lock()).Times(1);
+    
+    door.unlock();
+    door.lock();
+}
