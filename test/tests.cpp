9a01a552a46b556eec006f0b6c10eaf889e9d617
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-03-28 02:09:13 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,41 +8,50 @@ using ::testing::_;
 using ::testing::AtLeast;
 using ::testing::Throw;
 
+// Mocks for testing
 class MockTimerClient : public TimerClient {
- public:
+public:
     MOCK_METHOD(void, Timeout, (), (override));
 };
 
 class MockDoor : public Door {
- public:
+public:
     MOCK_METHOD(void, lock, (), (override));
     MOCK_METHOD(void, unlock, (), (override));
     MOCK_METHOD(bool, isDoorOpened, (), (override));
 };
 
+class MockTimer : public Timer {
+public:
+    MOCK_METHOD(void, tregister, (int, TimerClient*), ());
+};
+
+// Test Fixtures
 class TimedDoorTest : public ::testing::Test {
- protected:
-    TimedDoor* door;
-    int timeout = 1000;
-
+protected:
     void SetUp() override {
-        door = new TimedDoor(timeout);
+        door = new TimedDoor(5);
     }
-
     void TearDown() override {
         delete door;
     }
+    TimedDoor* door;
 };
 
-TEST_F(TimedDoorTest, DoorStartsLocked) {
+class AdapterTest : public ::testing::Test {
+protected:
+    MockDoor mockDoor;
+    DoorTimerAdapter adapter{*dynamic_cast<TimedDoor*>(&mockDoor)};
+};
+
+// Тесты для TimedDoor
+TEST_F(TimedDoorTest, InitialStateIsLocked) {
     EXPECT_FALSE(door->isDoorOpened());
 }
 
 TEST_F(TimedDoorTest, UnlockOpensDoor) {
-    testing::Mock::AllowLeak(door->adapter); // Игнорировать утечки моков
     door->unlock();
     EXPECT_TRUE(door->isDoorOpened());
-    // Убрать вызовы, которые могут вызвать исключение
 }
 
 TEST_F(TimedDoorTest, LockClosesDoor) {
@@ -51,46 +60,49 @@ TEST_F(TimedDoorTest, LockClosesDoor) {
     EXPECT_FALSE(door->isDoorOpened());
 }
 
-TEST_F(TimedDoorTest, TimeoutThrowsWhenOpen) {
+TEST_F(TimedDoorTest, ThrowWhenOpenTimeout) {
     door->unlock();
-    door->simulateTimeout();
     EXPECT_THROW(door->throwState(), std::runtime_error);
 }
 
 TEST_F(TimedDoorTest, NoThrowWhenClosed) {
+    door->lock();
     EXPECT_NO_THROW(door->throwState());
 }
 
-TEST(TimerTest, RegistersClient) {
+// Тесты для адаптера
+TEST_F(AdapterTest, TimeoutTriggersWhenOpen) {
+    EXPECT_CALL(mockDoor, isDoorOpened())
+        .WillOnce(::testing::Return(true));
+    EXPECT_THROW(adapter.Timeout(), std::runtime_error);
+}
+
+TEST_F(AdapterTest, NoActionWhenClosed) {
+    EXPECT_CALL(mockDoor, isDoorOpened())
+        .WillOnce(::testing::Return(false));
+    EXPECT_NO_THROW(adapter.Timeout());
+}
+
+// Тесты для Timer
+TEST(TimerTest, RegisterTimeout) {
     MockTimerClient client;
-    Timer timer;
-    EXPECT_CALL(client, Timeout()).Times(1);
-    timer.tregister(10, &client);
-}
-
-TEST(DoorTimerAdapterTest, CallsTimeout) {
-    TimedDoor door(1000);
-    door.unlock();
-    door.simulateTimeout();
-    EXPECT_THROW(door.throwState(), std::runtime_error);
-}
-TEST_F(TimedDoorTest, TimerRegistrationOnUnlock) {
-    MockTimerClient client;
-    Timer timer;
-    EXPECT_CALL(client, Timeout()).Times(1);
-    timer.tregister(1000, &client);
-}
-
-TEST_F(TimedDoorTest, ExceptionAfterTimeout) {
-    door->unlock();
-    EXPECT_THROW(door->adapter->Timeout(), std::runtime_error); // Проверка через адаптер
-}
-
-TEST(MockDoorTest, VerifyLockUnlock) {
-    MockDoor door;
-    EXPECT_CALL(door, unlock()).Times(1);
-    EXPECT_CALL(door, lock()).Times(1);
+    MockTimer timer;
     
+    EXPECT_CALL(client, Timeout()).Times(1);
+    EXPECT_CALL(timer, tregister(5, &client));
+    
+    timer.tregister(5, &client);
+}
+
+// Тесты исключений
+TEST(TimedDoorExceptionTest, InvalidTimeout) {
+    EXPECT_THROW(TimedDoor(-1), std::invalid_argument);
+    EXPECT_THROW(TimedDoor(0), std::invalid_argument);
+}
+
+TEST(TimedDoorIntegrationTest, FullWorkflow) {
+    TimedDoor door(1);
     door.unlock();
-    door.lock();
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    EXPECT_THROW(door.throwState(), std::runtime_error);
 }
