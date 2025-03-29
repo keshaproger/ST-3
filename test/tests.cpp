7d13216015194262e727e5deae176a1cd0e0371b
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-03-29 23:08:03 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,79 +1,91 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2024 SecureDoor Team
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
-#include <thread>
 #include "TimedDoor.h"
 
 using ::testing::_;
 using ::testing::AtLeast;
 
-class MockTimerSubscriber : public TimerSubscriber {
- public:
-  MOCK_METHOD(void, OnTimerExpired, (), (override));
+// Мок-классы для тестирования
+class MockTimerClient : public TimerClient {
+public:
+    MOCK_METHOD(void, OnTimeout, (), (override));
 };
 
-class SecurityTest : public ::testing::Test {
- protected:
-  void SetUp() override { door = new TimedSmartDoor(3); }
-  void TearDown() override { delete door; }
-  
-  TimedSmartDoor* door;
+class MockDoor : public Door {
+public:
+    MOCK_METHOD(void, Close, (), (override));
+    MOCK_METHOD(void, Open, (), (override));
+    MOCK_METHOD(bool, IsOpen, (), (const, override));
 };
 
-TEST_F(SecurityTest, DefaultSecurityState) {
-  EXPECT_FALSE(door->CheckStatus());
-  EXPECT_EQ(door->GetDuration(), 3);
+class TimedDoorTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        door = new TimedDoor(3);
+    }
+    void TearDown() override {
+        delete door;
+    }
+    TimedDoor* door;
+};
+
+TEST_F(TimedDoorTest, InitializationTest) {
+    EXPECT_FALSE(door->IsOpen());
+    EXPECT_EQ(door->GetTimeout(), 3);
 }
 
-TEST_F(SecurityTest, DoorActivationSequence) {
-  door->Secure();
-  door->Release();
-  EXPECT_TRUE(door->CheckStatus());
+TEST_F(TimedDoorTest, OpenDoorStartsTimer) {
+    door->Open();
+    EXPECT_TRUE(door->IsOpen());
 }
 
-TEST_F(SecurityTest, EmergencyLockProcedure) {
-  door->Release();
-  door->Secure();
-  EXPECT_FALSE(door->CheckStatus());
+TEST_F(TimedDoorTest, CloseDoorResetsState) {
+    door->Open();
+    door->Close();
+    EXPECT_FALSE(door->IsOpen());
 }
 
-TEST_F(SecurityTest, SecurityBreachScenario) {
-  door->Release();
-  EXPECT_THROW(door->TriggerSecurityCheck(), std::logic_error);
+TEST_F(TimedDoorTest, TimeoutTriggersExceptionWhenOpen) {
+    door->Open();
+    EXPECT_THROW(door->CheckState(), std::logic_error);
 }
 
-TEST_F(SecurityTest, ConsecutiveAccessAttempts) {
-  door->Release();
-  door->Release();
-  EXPECT_TRUE(door->CheckStatus());
+TEST_F(TimedDoorTest, NoExceptionWhenClosed) {
+    door->Close();
+    EXPECT_NO_THROW(door->CheckState());
 }
 
-TEST_F(SecurityTest, InvalidTimerConfiguration) {
-  EXPECT_THROW(TimedSmartDoor invalid_door(0), std::invalid_argument);
+TEST_F(TimedDoorTest, InvalidTimeoutThrowsOnCreation) {
+    EXPECT_THROW(TimedDoor invalidDoor(0), std::invalid_argument);
+    EXPECT_THROW(TimedDoor invalidDoor(-5), std::invalid_argument);
 }
 
-TEST_F(SecurityTest, PostLockInspection) {
-  door->Secure();
-  EXPECT_NO_THROW(door->TriggerSecurityCheck());
+TEST_F(TimedDoorTest, MultipleOpenCallsKeepDoorOpen) {
+    door->Open();
+    door->Open(); // Повторный вызов
+    EXPECT_TRUE(door->IsOpen());
 }
 
-TEST(TimerIntegrationTest, SubscriberNotification) {
-  MockTimerSubscriber mock_sub;
-  TimerScheduler scheduler;
-
-  EXPECT_CALL(mock_sub, OnTimerExpired()).Times(1);
-  scheduler.ScheduleTimer(0, &mock_sub);
+TEST(AdapterTest, AdapterCallsCheckState) {
+    TimedDoor door(2);
+    TimerAdapter adapter(door);
+    door.Open();
+    EXPECT_THROW(adapter.OnTimeout(), std::logic_error);
 }
 
-TEST(HandlerTest, ClosedDoorNoAlert) {
-  TimedSmartDoor test_door(2);
-  test_door.Secure();
-  EXPECT_NO_THROW(test_door.TriggerSecurityCheck());
+TEST(TimerTest, TimerCallsClientMethod) {
+    MockTimerClient client;
+    EXPECT_CALL(client, OnTimeout()).Times(1);
+    
+    Timer timer;
+    timer.SetTimer(0, &client); // Немедленный вызов
 }
 
-TEST(HandlerTest, ImmediateTimeoutResponse) {
-  TimedSmartDoor test_door(1);
-  test_door.Release();
-  std::this_thread::sleep_for(std::chrono::seconds(1));
-  EXPECT_THROW(test_door.TriggerSecurityCheck(), std::logic_error);
+TEST(TimerIntegrationTest, FullFlowTest) {
+    TimedDoor door(1);
+    door.Open();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    EXPECT_THROW(door.CheckState(), std::logic_error);
 }
